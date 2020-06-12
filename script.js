75d1eed3faf01cb13f1521f279cc842d7f8c47d1
maliccny <maliccny@gmail.com>
2020-06-12 16:26:08 -0400

    fixed youtube

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ async function getAnime() {
 
   removeInfo()
   const sectionInfo = document.querySelector('#section-info');
+  let videoTag = document.querySelector('iframe')
   let id = Math.ceil(Math.random() * Math.floor(100));
   let animeObj = await axios.get(`https://kitsu.io/api/edge/anime/${id}`);
-
+  let titleOpen = `${animeObj.data.data.attributes.canonicalTitle} Opening`
+  console.log(titleOpen)
+  let vidObj = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${titleOpen}&key=AIzaSyCdNw0Cahn4NwywdgWzL802Nwnk3UTUWxY`);
+  let videoCode = vidObj.data.items[0].id.videoId;
+  console.log(videoCode);
 
   //printing of info in console, testing purposes
 
@@ -44,6 +49,12 @@ async function getAnime() {
     </div>`;
   
   // sectionInfo.innerHTML += `<p>Summary: ${animeObj.data.data.attributes.synopsis}</p>`;
+
+  
+  console.log(videoTag)
+  console.log(videoTag.src)
+  videoTag.src = `https://www.youtube.com/embed/${videoCode}?autoplay=1`
+  console.log(videoTag.src)
   
 }
 
